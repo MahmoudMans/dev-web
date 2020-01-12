0dbcef491113bb99da029ed4503964a32f97ad08
MahmoudMans <57843834+MahmoudMans@users.noreply.github.com>
2020-01-12 12:15:40 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -82,3 +82,67 @@ function under() {
         $("#img33").find(".btnCourses1").toggleClass('btnCourses1Hover')
     })
 
+    var slideIndex = 1;
+    if (document.getElementsByClassName("mySlides").length>0)
+    showSlides(slideIndex);
+    
+    // Next/previous controls
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    // Thumbnail image controls
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+    }
+    
+  var firebaseConfig = {
+    apiKey: "AIzaSyBOfart0RIx5x9YIZJgcDvSg11hceD3SsQ",
+    authDomain: "devweb1-d9807.firebaseapp.com",
+    databaseURL: "https://devweb1-d9807.firebaseio.com",
+    projectId: "devweb1-d9807",
+    storageBucket: "devweb1-d9807.appspot.com",
+    messagingSenderId: "933172181565",
+    appId: "1:933172181565:web:2f10c4574468f0db010854",
+    measurementId: "G-B2DP5SS537"
+  };
+ 
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  document.getElementById('contactForm').addEventListener('submit',submitForm);
+var messageRef = firebase.database().ref('myDatabase');
+function submitForm(e){
+    e.preventDefault();
+    var name= document.getElementById('name').value;
+    var email= document.getElementById('email').value;
+    var phone= document.getElementById('phone').value;
+    
+    console.log(name+email+phone)
+    saveMessage(name,email,phone);
+}
+
+  function saveMessage(name,email,phone){
+      var newMessageRef = messageRef.push();
+      newMessageRef.set({
+          name: name,
+          email:email,
+          phone:phone,
+         
+      });
+  }
